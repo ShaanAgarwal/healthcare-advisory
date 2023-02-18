e112605b7b21d1fa701b0a29d9417e5287ac91db
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2023-02-18 20:12:55 +0530

    Hover effect inserted

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import "./Home.css";
+import CervialFractureImage from "./img.png";
 
 function Home() {
   return (
+    <>
     <div>Home</div>
+    <div class="card">
+        <div class="circle"></div>
+        <div class="content">
+           <h2>Cervical Fracture Detection</h2>
+           <p>Loren ipsum akjdfasldkfja sdflkajsd flakdsfa;sdkflaja;sdlkf a;sdlkfja s;dflkja ds;fjk 
+            lakdjfa</p>
+            <a href="#">Test Now</a>
+        </div>
+        <img src={CervialFractureImage} alt="image" />
+    </div>
+    </>
   )
 }
 
